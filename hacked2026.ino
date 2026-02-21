c008bfb47e3461b2c9bb62cb5acd72d0d857ae88
nmackenzy <n.mackenzy@outlook.com>
2026-02-20 19:19:40 -0700

    to be tested

--- hacked2026.ino
+++ hacked2026.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+// #include "sensor.h"
+#include "OLED.h"
+#include "buzzer.h"
+#include "app.h"
+#include "fakeData.h"
 
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  // setupSensor();
+  setupOLED();
+  setupBuzzer();
+  setupApp();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // test
+  unsigned long now = millis();
+  static unsigned long lastSensor = 0;
+  static unsigned long lastSend   = 0;
+  static float currentGAS1PPM    = 0;
+  static float currentGAS2PPM    = 0;
+
+  if (now - lastSensor > 1000) {
+    currentGAS1PPM = getGAS1PPM();
+    currentGAS2PPM = getGAS2PPM();
+
+    Serial.print("GAS1: "); Serial.print(currentGAS1PPM, 1); Serial.println(" PPM");
+    Serial.print("GAS2: "); Serial.print(currentGAS2PPM, 1); Serial.println(" PPM");
+
+    updateDisplay(currentGAS1PPM, currentGAS2PPM);
+    checkAlarm(currentGAS1PPM, currentGAS2PPM);
+    lastSensor = now;
+  }
+
+  if (now - lastSend > 5000) {
+    sendDataToApp(currentGAS1PPM, currentGAS2PPM);
+    lastSend = now;
+  }
 }
